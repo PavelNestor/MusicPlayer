b18a2273e454b9c9cec5e9fd6230e11f38ea7930
NestorPavel <wdirect@ya.ru>
2018-06-20 08:31:25 +0300

    Start!

---
+++ src/main/java/com/gavialis/app/amp/service/PlayerService.java
@@ -0,0 +1,29 @@
+package com.gavialis.app.amp.service;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.media.MediaBrowserCompat;
+import android.support.v4.media.MediaBrowserServiceCompat;
+
+import java.util.List;
+
+/**
+ * Created by Chief on 20.06.2018.
+ */
+
+public class PlayerService extends MediaBrowserServiceCompat {
+
+
+
+    @Nullable
+    @Override
+    public BrowserRoot onGetRoot(@NonNull String clientPackageName, int clientUid, @Nullable Bundle rootHints) {
+        return null;
+    }
+
+    @Override
+    public void onLoadChildren(@NonNull String parentId, @NonNull Result<List<MediaBrowserCompat.MediaItem>> result) {
+
+    }
+}
